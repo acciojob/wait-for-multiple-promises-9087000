838f3752cae01b97d9873be891896b58e5fd144f
9087000 <50361103+9087000@users.noreply.github.com>
2024-10-19 14:38:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const pro1=()=>{
+	let time = Math.random() * 2000 + 1000;
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(['Promise 1',time])
+        },time)
+    })
+}
+
+const pro2=()=>{
+	let time = Math.random() * 2000 + 1000;
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(["Promise 2",time])
+        },2002)
+    })
+}
+
+
+const pro3=()=>{
+	let time = Math.random() * 2000 + 1000;
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(["Promise 3",time])
+        },2052)
+    })
+}
+let body=document.getElementById("output");
+function solve(){
+    let obj1=new Date();
+    console.log(obj1);
+    Promise.all([pro1(),pro2(),pro3()]).then((data)=>{
+        console.log(data);
+        let obj2=new Date();
+        console.log(obj2);
+        body.innerHTML="";
+        data.forEach(element => {
+            console.log(element);
+            body.innerHTML+=`<tr>
+            <th>${element[0]}</th>
+            <th>${element[1]/1000}</th>
+        </tr>`
+        });
+        body.innerHTML+=` <tr>
+        <th>Total</th>
+        <th>${(obj2-obj1)/1000}</th>
+    </tr>`
+    })
+}
+solve();
